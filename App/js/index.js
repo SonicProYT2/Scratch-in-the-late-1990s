4303132017ffb0c2472f00510d95cbd55bce30ca
Charles Cat Alien <72233418+SonicProYT2@users.noreply.github.com>
2020-12-13 18:40:05 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,17 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("53.333333333333336em")
+                .setTop("0.7619047619047619em")
+                .setWidth("4.571428571428571em")
+                .setHeight("4.114285714285714em")
+                .setSrc("{/}img/default-avatar.gif")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
