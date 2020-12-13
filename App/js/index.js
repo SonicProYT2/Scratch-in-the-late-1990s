4c8a719f324b5ce24e4a90561336aa25a8ecab55
Charles Cat Alien <72233418+SonicProYT2@users.noreply.github.com>
2020-12-13 18:48:51 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -35,9 +35,93 @@ xui.Class('App', 'xui.Module',{
                 .setHost(host,"xui_ui_image7")
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("9.447619047619048em")
-                .setHeight("7.923809523809524em")
+                .setWidth("7.161904761904762em")
+                .setHeight("6.4em")
                 .setSrc("{/}img/scratch beta logo.png")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button10")
+                .setDirtyMark(false)
+                .setLeft("6.095238095238095em")
+                .setTop("0.7619047619047619em")
+                .setCaption("File")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button11")
+                .setDirtyMark(false)
+                .setLeft("9.142857142857142em")
+                .setTop("0.7619047619047619em")
+                .setCaption("Edit")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button12")
+                .setDirtyMark(false)
+                .setLeft("12.19047619047619em")
+                .setTop("0.7619047619047619em")
+                .setCaption("Insert")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button13")
+                .setDirtyMark(false)
+                .setLeft("16em")
+                .setTop("0.7619047619047619em")
+                .setCaption("View")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input6")
+                .setDirtyMark(false)
+                .setLeft("17.523809523809526em")
+                .setTop("0.7619047619047619em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Project Name")
+            );
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput18")
+                .setDirtyMark(false)
+                .setLeft("0em")
+                .setTop("43.42857142857143em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Color Picker")
+                .setType("color")
+            );
+            
+            append(
+                xui.create("xui.UI.RichEditor")
+                .setHost(host,"xui_ui_richeditor1")
+                .setDirtyMark(false)
+                .setLeft("35.04761904761905em")
+                .setTop("3.8095238095238093em")
+                .setWidth("21.333333333333332em")
+                .setHeight("26em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("Project Editor")
+                .setLabelHAlign("left")
+            );
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel5")
+                .setDock("none")
+                .setLeft("0.7619047619047619em")
+                .setTop("6.857142857142857em")
+                .setWidth("28.266666666666666em")
+                .setHeight("22.933333333333334em")
+                .setCaption("Preview")
             );
             
             return children;
